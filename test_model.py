c733ea522702d9e1941122524cf0434880f0c14e
senKo183 <lehungnlb@gmail.com>
2026-05-03 16:32:06 +0700

    Fixed test_model.py

--- test_model.py
+++ test_model.py
@@ -3,8 +3,11 @@ import json
 with open("metrics.json") as f:
     metrics = json.load(f)
 
-accuracy = metrics["accuracy"]
+rmse = metrics["rmse"]
+r2 = metrics["r2"]
 
-assert accuracy > 0.8
+# Điều kiện test
+assert rmse < 1.0, "RMSE too high"
+assert r2 > 0.7, "R2 too low"
 
 print("Model test passed")
